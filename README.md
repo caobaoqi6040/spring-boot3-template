09124180485a7d64bfeb56586164528b754dceef
caobaoqi6040 <caobaoqi6040@gmail.com>
2025-08-02 20:01:30 +0800

    🎉 init: 好的开始

---
+++ README.md
@@ -0,0 +1,4 @@
+# spring-boot3-template
+
+- https://pan.quark.cn/s/d5ff4b6f57ac
+- https://pan.quark.cn/s/9362d4543bf5
---
+++ src/main/java/dev/caobaoqi6040/backend/SpringBoot3TemplateApplication.java
@@ -0,0 +1,21 @@
+package dev.caobaoqi6040.backend;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+@Slf4j
+@SpringBootApplication
+public class SpringBoot3TemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3TemplateApplication.class, args);
+    }
+
+    @EventListener
+    public void onApplicationReady(ApplicationReadyEvent event) {
+        log.info("event = {}", event);
+    }
+}
