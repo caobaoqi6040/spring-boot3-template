a7532740eabf5a4b2a347ab9a0c21dc1aeb6c67f
caobaoqi6040 <caobaoqi6040@gmail.com>
2025-09-04 12:09:50 +0800

    :beers: feat(config): add BlockAttackInnerInterceptor for mp

--- src/main/java/dev/caobaoqi6040/backend/config/MybatisPlusConfiguration.java
+++ src/main/java/dev/caobaoqi6040/backend/config/MybatisPlusConfiguration.java
@@ -3,6 +3,7 @@ package dev.caobaoqi6040.backend.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,30 +19,31 @@ import java.time.LocalDateTime;
 @MapperScan("dev.caobaoqi6040.backend.*.mapper")
 public class MybatisPlusConfiguration implements MetaObjectHandler {
 
-    @Override
-    public void insertFill(MetaObject metaObject) {
-        log.info("MybatisPlusConfiguration.insertFill start");
-        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
-        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
-        log.info("MybatisPlusConfiguration.insertFill end");
-    }
+	@Override
+	public void insertFill(MetaObject metaObject) {
+		log.info("MybatisPlusConfiguration.insertFill start");
+		this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
+		this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+		log.info("MybatisPlusConfiguration.insertFill end");
+	}
 
-    @Override
-    public void updateFill(MetaObject metaObject) {
-        log.info("MybatisPlusConfiguration.updateFill start");
-        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
-        log.info("MybatisPlusConfiguration.updateFill end");
-    }
+	@Override
+	public void updateFill(MetaObject metaObject) {
+		log.info("MybatisPlusConfiguration.updateFill start");
+		this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+		log.info("MybatisPlusConfiguration.updateFill end");
+	}
 
-    /**
-     * 插件配置
-     */
-    @Bean
-    public MybatisPlusInterceptor mybatisPlusInterceptor() {
-        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL)); // 如果配置多个插件, 切记分页最后添加
-        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
-        return interceptor;
-    }
+	/**
+	 * 插件配置
+	 */
+	@Bean
+	public MybatisPlusInterceptor mybatisPlusInterceptor() {
+		MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+		interceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
+		interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+		interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL)); // 如果配置多个插件, 切记分页最后添加
+		return interceptor;
+	}
 
 }
