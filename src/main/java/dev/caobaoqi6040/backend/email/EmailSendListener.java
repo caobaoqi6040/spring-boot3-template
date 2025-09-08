22f5de0d3c542b6a87008c4df1fb0e2ee708bbc1
caobaoqi6040 <caobaoqi6040@gmail.com>
2025-09-08 17:25:41 +0800

    :beers: feat(sample): annotation sample

--- src/main/java/dev/caobaoqi6040/backend/email/EmailSendListener.java
+++ src/main/java/dev/caobaoqi6040/backend/email/EmailSendListener.java
@@ -1,7 +1,7 @@
 package dev.caobaoqi6040.backend.email;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 /**
@@ -12,10 +12,10 @@ import org.springframework.stereotype.Component;
  */
 @Slf4j
 @Component
-public class EmailSendListener implements ApplicationListener<EmailSendEvent> {
+public class EmailSendListener {
 
-	@Override
-	public void onApplicationEvent(EmailSendEvent event) {
+	@EventListener
+	public void onEmailSendEvent(EmailSendEvent event) {
 		log.info("email {} send with {}", event.getId(), event.getCreateTime());
 	}
 
